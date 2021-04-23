4e8ca9abcb25fdf4d0bbdff9ed27b4b735c3dca2
Joe Lyons <josephjlyons90@gmail.com>
2021-04-23 18:01:37 -0400

    onclick and state functions added

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 
 
 
-const ExpenseItem = (props: { date: { toLocaleString: (arg0: string, arg1: { month?: string | undefined; day?: string | undefined; }) => any; getFullYear: () => any; }; title: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; amount: string | number | boolean | {} | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactNodeArray | React.ReactPortal | null | undefined; }): JSX.Element => {
-   
-   return (
+const ExpenseItem = (props: { date: { toLocaleString: (arg0: string, arg1: { month?: string; day?: string }) => any; getFullYear: () => any; }; title: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; amount: string | number | boolean | {} | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactNodeArray | React.ReactPortal | null | undefined; }): JSX.Element => {
+
+    const [title, setTitle] = useState(props.title);
+    console.log('ExpenseItem evaluted by React')
+
+
+    const clickHandler = () => {
+        setTitle('Updated');
+        console.log(title);
+    }
+    return (
         <Card className="expense-item">
-          <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
             </div>
             <div className="expense-item__price">${props.amount}</div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
