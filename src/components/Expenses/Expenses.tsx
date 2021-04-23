8bd825107309d33a35703a7d7e8128606f70a3cd
Joe Lyons <josephjlyons90@gmail.com>
2021-04-23 19:17:25 -0400

    filter year connection

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card'
+import ExpenseFilter from './ExpenseFilter'
 import ExpenseItem from './ExpenseItem'
 
+
 const Expenses = (props: { items: { title: string; amount: number; date: any; }[]; }) => {
+  
+  const [filteredYear, setFiltedYear] = useState('2021')
+  
+  const filterChangeHandler = (SelectedYear: any) => {
+    setFiltedYear(SelectedYear)
+
+  }
   return (
-    <Card>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+
+      <Card>
+        <ExpenseFilter selected={filteredYear} onFilterYear={filterChangeHandler} />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   )
 }
 
